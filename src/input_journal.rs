e168a02ea43d1b23f330d2f86e6349609e474321
Alexey <4681325+qqrm@users.noreply.github.com>
2026-02-26 15:33:48 +0300

    Fix test-only push_text re-export in input_journal

--- src/input_journal.rs
+++ src/input_journal.rs
@@ -1,5 +1,8 @@
+#[cfg(test)]
+#[allow(unused_imports)]
+pub use crate::input::ring_buffer::push_text;
 #[allow(unused_imports)]
 pub use crate::input::ring_buffer::{
     InputRun, LayoutTag, RunKind, RunOrigin, mark_last_token_autoconverted, push_run, push_runs,
-    push_text, take_last_layout_run_with_suffix,
+    take_last_layout_run_with_suffix,
 };
